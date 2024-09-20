147e431a4ba9c3dbb87818b5b49e7c4d113ab628
gyeongmann <hkc8857@naver.com>
2024-09-20 15:39:02 +0900

    \#47 feat: 임시 로그인 데이터 및 로직 구현

---
+++ fit-card/interfaces/User.ts
@@ -0,0 +1,9 @@
+// @/interfaces/User.ts
+
+export interface User {
+  id: string;
+  loginId: string;
+  name: string;
+  birthDate: string;
+  phoneNumber: string;
+}
---
+++ fit-card/mock/mockUser.ts
@@ -0,0 +1,8 @@
+export const mockUser = {
+  id: "1",
+  loginId: "test",
+  password: "1234",
+  name: "핏카츄",
+  birthDate: "1990-01-01",
+  phoneNumber: "010-1234-5678",
+};
