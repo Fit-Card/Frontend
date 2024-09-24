a6832ae5ab8416a84d8e3a4db88efa9778f522ba
gyeongmann <hkc8857@naver.com>
2024-09-24 12:25:45 +0900

    #46 feat: 카드 추천 사용자 정보 불러오기

--- fit-card/components/recommend/ConsumptionPattern.tsx
+++ fit-card/components/recommend/ConsumptionPattern.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { User } from "@/interfaces/User";
 
-function ConsumptionPattern() {
+function ConsumptionPattern({ user }: { user: User }) {
   return (
     <View>
-      <Text>소비패턴</Text>
+      {/* 사용자 이름과 소비 패턴 텍스트 표시 */}
+      <Text style={styles.userName}>{user.name}님의 소비 패턴</Text>
     </View>
   );
 }
 
 export default ConsumptionPattern;
+
+const styles = StyleSheet.create({
+  userName: {
+    fontSize: 20,
+    fontFamily: "SUITE-Bold",
+  },
+});
