82a06f857f659fc30dcc3f477a9ab06e02950cf8
gyeongmann <hkc8857@naver.com>
2024-09-24 13:56:19 +0900

    #46 feat: 카드 추천 페이지 문구 추가 및 정렬

--- fit-card/pages/Card.tsx
+++ fit-card/pages/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Text } from "react-native";
 import ConsumptionPattern from "@/components/recommend/ConsumptionPattern";
 import RecommendedCard from "@/components/recommend/RecommendedCard";
 import AgeGroupCard from "@/components/recommend/AgeGroupCard";
@@ -17,13 +17,19 @@ export default function CardScreen() {
       </View>
 
       {/* 추천 카드 Component */}
-      <View style={styles.inner}>
-        <RecommendedCard />
+      <View style={styles.componentWrapper}>
+        <Text style={styles.headText}>{user!.name}님 소피 패턴에 맞춘 카드를 추천해드릴게요.</Text>
+        <View style={styles.inner}>
+          <RecommendedCard />
+        </View>
       </View>
 
       {/* 연령대의 카드 Component */}
-      <View style={styles.inner}>
-        <AgeGroupCard />
+      <View style={styles.componentWrapper}>
+        <Text style={styles.headText}>{user!.name}님 나이대에서 많이 사용하는 카드</Text>
+        <View style={styles.inner}>
+          <AgeGroupCard />
+        </View>
       </View>
     </ScrollView>
   );
@@ -36,6 +42,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F7F7F7", // 배경색 설정
     padding: 20, // 여백 설정
+  },
+  componentWrapper: {
+    width: "100%", // 각 컴포넌트를 감싸는 View의 너비를 전체로 설정
+    alignItems: "center", // 컴포넌트 중앙 정렬
   },
   inner: {
     width: "90%", // 상자의 너비 (화면의 90%)
@@ -51,5 +61,11 @@ const styles = StyleSheet.create({
     minHeight: 150, // 각 컴포넌트 상자의 최소 높이
     justifyContent: "center", // 상자 내부의 컴포넌트 세로 정렬
     alignItems: "center", // 상자 내부의 컴포넌트 가로 정렬
+  },
+  headText: {
+    fontFamily: "SUITE-Bold",
+    marginBottom: 10,
+    alignSelf: "flex-start", // 텍스트 왼쪽 정렬
+    marginLeft: "5%",
   },
 });
