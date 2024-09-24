fe32b0e5952d1390fc14eae5f5a44a94c06a87db
gyeongmann <hkc8857@naver.com>
2024-09-24 17:15:37 +0900

    #46 chore: 카드추천 화면 수정

--- fit-card/pages/Card.tsx
+++ fit-card/pages/Card.tsx
@@ -12,14 +12,13 @@ export default function CardScreen() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {/* 소비패턴 Component */}
-      <View style={styles.componentWrapper}>
+      <View style={styles.sharedBackground}>
+        {/* 소비패턴 Component */}
         <View style={styles.inner}>
           <ConsumptionPattern user={user!} />
         </View>
-      </View>
 
-      {/* 추천 카드 Component */}
-      <View style={styles.componentWrapper}>
+        {/* 추천 카드 Component */}
         <Text style={styles.headText}>{user!.name}님 소피 패턴에 맞춘 카드를 추천해드릴게요.</Text>
         <View style={styles.inner}>
           <RecommendedCard />
@@ -67,10 +66,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#F7F7F7", // 배경색 설정
     padding: 20, // 여백 설정
   },
-  componentWrapper: {
-    width: "100%", // 각 컴포넌트를 감싸는 View의 너비를 전체로 설정
-    alignItems: "center", // 컴포넌트 중앙 정렬
-    marginBottom: 10,
+  sharedBackground: {
+    width: "90%", // 전체 너비로 설정
+    backgroundColor: "#FFFFFF", // 공통 배경색
+    padding: 20, // 상자 내부 여백
+    borderRadius: 10, // 모서리 둥글게
+    shadowColor: "#000", // 그림자 효과
+    shadowOffset: { width: 0, height: 2 }, // 그림자 위치
+    shadowOpacity: 0.1, // 그림자 불투명도
+    shadowRadius: 4, // 그림자 반경
+    elevation: 3, // 안드로이드 그림자 효과
+    marginBottom: 20, // 아래 컴포넌트와 간격
   },
   ageComponentWrapper: {
     width: "100%", // 각 컴포넌트를 감싸는 View의 너비를 전체로 설정
@@ -79,15 +85,6 @@ const styles = StyleSheet.create({
   },
   inner: {
     width: "90%", // 상자의 너비 (화면의 90%)
-    backgroundColor: "#FFFFFF", // 상자의 배경색
-    padding: 20, // 상자 내부 여백
-    paddingBottom: 15,
-    borderRadius: 10, // 상자 모서리 둥글게
-    shadowColor: "#000", // 그림자 효과
-    shadowOffset: { width: 0, height: 2 }, // 그림자 위치
-    shadowOpacity: 0.1, // 그림자 불투명도
-    shadowRadius: 4, // 그림자 반경
-    elevation: 3, // 안드로이드 그림자 효과
     marginBottom: 10, // 컴포넌트 간의 간격
     minHeight: 150, // 각 컴포넌트 상자의 최소 높이
     justifyContent: "center", // 상자 내부의 컴포넌트 세로 정렬
