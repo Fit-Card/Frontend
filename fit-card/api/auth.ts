3510bfecfab54c810cdf74d2d9c3eb949451ff62
gyeongmann <hkc8857@naver.com>
2024-09-27 12:15:43 +0900

    #66 feat: 회원가입, 아이디 중복 api

---
+++ fit-card/api/auth.ts
@@ -0,0 +1,33 @@
+// @/api/auth.ts
+import axios from "axios";
+import { SignupUser } from "@/interfaces/User";
+import { formatDate } from "@/handlers/inputHandlers";
+
+export const checkid = async (userId: string) => {
+  try {
+    const response = await axios.post(`http://j11a405.p.ssafy.io:8081/auth/checkid`, {
+      loginId: userId,
+    });
+    console.log(response.data);
+    return response.data.data.duplicated; // 서버에서 반환한 데이터
+  } catch (error) {
+    console.error("Error checking user ID:", error);
+    throw error;
+  }
+};
+
+export const register = async (user: SignupUser) => {
+  try {
+    const response = await axios.post(`http://j11a405.p.ssafy.io:8081/auth/register`, {
+      loginId: user.loginId,
+      password: user.password,
+      name: user.name,
+      phoneNumber: user.phoneNumber,
+      birthDate: formatDate(user.birthDate),
+    });
+    return response.data.message; // 서버에서 반환한 데이터
+  } catch (error) {
+    console.error("Error register user:", error);
+    throw error;
+  }
+};
