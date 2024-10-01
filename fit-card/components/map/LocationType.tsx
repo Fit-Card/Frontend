6a435f6383e99e9ef5d41ed0d5e747cf68b3f49d
김현지 <chik1y@naver.com>
2024-10-01 16:57:37 +0900

    #71 지도 검색에 필요한 타입 지정

--- fit-card/components/map/LocationType.tsx
+++ fit-card/components/map/LocationType.tsx
@@ -17,3 +17,17 @@ export type SelectedLocationType = {
   kakaoUrl: string;
   category: number;
 };
+
+export type Category = {
+  id: number;
+  title: string;
+  code: string;
+};
+
+export const categories: Category[] = [
+  { id: 0, title: "음식점", code: "FD6" },
+  { id: 1, title: "카페", code: "CE7" },
+  { id: 2, title: "편의점", code: "CS2" },
+  { id: 3, title: "문화시설", code: "CT1" },
+  { id: 4, title: "주유소", code: "OL7" },
+];
